4f1e9c265e6d83bcd4f5243c5e4822d208ead085
busekaraman <buse.karaman414@gmail.com>
2021-12-17 17:10:12 +0300

    user tablosu düzenlendi

--- lib/model/user_model.dart
+++ lib/model/user_model.dart
@@ -3,24 +3,37 @@ class UserModel {
   String? email;
   String? firstName;
   String? secondName;
+  String? home_id;
+  double? moneyState;
 
-  UserModel({this.uid, this.email, this.firstName, this.secondName});
+  UserModel(
+      {this.uid,
+      this.email,
+      this.firstName,
+      this.secondName,
+      this.home_id = '0',
+      this.moneyState = 0});
 
+  //veriyi çekme
   factory UserModel.fromMap(map) {
     return UserModel(
-      uid: map['uid'],
-      email: map['email'],
-      firstName: map['firstName'],
-      secondName: map['secondName'],
-    );
+        uid: map['uid'],
+        email: map['email'],
+        firstName: map['firstName'],
+        secondName: map['secondName'],
+        home_id: map['home_id'],
+        moneyState: map['moneyState']);
   }
 
+  //veri yollama
   Map<String, dynamic> toMap() {
     return {
       'uid': uid,
       'email': email,
       'firstName': firstName,
       'secondName': secondName,
+      'home_id': home_id,
+      'moneyState': moneyState
     };
   }
 }
